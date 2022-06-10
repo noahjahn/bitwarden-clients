7e5428bc51c18629b1e408b6fc9cf7d0e1c80899
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-06-10 08:21:12 -0400

    trial component params

--- apps/web/src/app/modules/trial/trial.component.ts
+++ apps/web/src/app/modules/trial/trial.component.ts
@@ -1,9 +1,21 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { first } from "rxjs";
 
 @Component({
   selector: "app-trial",
   templateUrl: "trial.component.html",
 })
-export class TrialComponent {
+export class TrialComponent implements OnInit {
   layout = "";
+
+  constructor(router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.pipe(first()).subscribe((qParams) => {
+      if (qParams.layout != null) {
+        this.layout = qParams.layout;
+      }
+    });
+  }
 }
