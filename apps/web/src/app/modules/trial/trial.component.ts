e0b1e3262611d2da4316d32aa5c19254446b9ad3
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-06-14 17:30:47 -0400

    Support teams, enterprise, and families layout param and more layout ui work

--- apps/web/src/app/modules/trial/trial.component.ts
+++ apps/web/src/app/modules/trial/trial.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { first } from "rxjs";
 
@@ -8,11 +8,15 @@ import { first } from "rxjs";
 })
 export class TrialComponent implements OnInit {
   layout = "";
+  email = "";
 
   constructor(router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.queryParams.pipe(first()).subscribe((qParams) => {
+      if (qParams.email != null && qParams.email.indexOf("@") > -1) {
+        this.email = qParams.email;
+      }
       if (qParams.layout != null) {
         this.layout = qParams.layout;
       }
