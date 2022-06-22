b77c97cf0568d3c680dcc1c7c3be35bec2e23386
addison <addisonbeck1@gmail.com>
2022-06-22 14:18:58 -0400

    [fix] Simplify ThemeBuilder's constructor

--- libs/angular/src/services/theming/themeBuilder.ts
+++ libs/angular/src/services/theming/themeBuilder.ts
@@ -3,17 +3,11 @@ import { ThemeType } from "@bitwarden/common/enums/themeType";
 import { Theme } from "./theme";
 
 export class ThemeBuilder implements Theme {
-  readonly configuredTheme: ThemeType;
-  readonly systemTheme: ThemeType;
-
   get effectiveTheme(): ThemeType {
     return this.configuredTheme != ThemeType.System ? this.configuredTheme : this.systemTheme;
   }
 
-  constructor(themeSetting: ThemeType, systemTheme: ThemeType) {
-    this.configuredTheme = themeSetting;
-    this.systemTheme = systemTheme;
-  }
+  constructor(readonly configuredTheme: ThemeType, readonly systemTheme: ThemeType) {}
 
   updateSystemTheme(systemTheme: ThemeType): ThemeBuilder {
     return new ThemeBuilder(this.configuredTheme, systemTheme);
